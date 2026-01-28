b305f90be68773fe6752609be26264b48246c00c
Nguyendinhnam28803 <nguyendinhnam241209@gmail.com>
2026-01-28 09:11:44 +0700

    Add model Auth

---
+++ YC4_THUC_TAP_SINH_BE/Dto/AuthDto.cs
@@ -0,0 +1,25 @@
+﻿namespace YC4_THUC_TAP_SINH_BE.Dto
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        public string token { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class RegisterRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
